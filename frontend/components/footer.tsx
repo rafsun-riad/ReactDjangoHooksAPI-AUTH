56f50b41e17eec67d12bbca91bcd05c123a443fb
rafsun-riad <mruhaquer@gmail.com>
2025-07-15 13:04:47 +0600

    update: light and dark mode change added

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <div>
-      <footer className="bg-blue-500 text-white py-4 mt-8">
+      <footer className="bg-blue-500 text-white py-4 mt-8 dark:bg-gray-800 dark:text-gray-200">
         <div className="container mx-auto text-center">
           <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
           <p>
--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -6,72 +6,77 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { FaRegUser } from "react-icons/fa6";
-import Link from "next/link";
 import { useAuth } from "@/hooks/useAuth";
-import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FaRegUser } from "react-icons/fa6";
+import DarkLightSwitch from "./dark-light-switch";
 
 function Header() {
   const router = useRouter();
   const { user } = useAuth();
   const { logout } = useAuth();
+
   function handleLogout() {
     logout();
     router.push("/login");
   }
   return (
-    <div className="bg-blue-500 text-white p-4">
+    <div className="bg-blue-500 text-white p-4 dark:bg-gray-800 dark:text-gray-200">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <Link href="/">
             <h1 className="text-2xl font-bold">My Blog</h1>
           </Link>
-          {user ? (
-            <div>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button className="bg-white text-blue-500 hover:bg-gray-100 hover:text-blue-700">
-                    <FaRegUser className="mr-2" />
-                    Profile
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-48">
-                  <div className="text-center">
-                    <ul className="space-y-2">
-                      <li>
-                        <Link
-                          href="/category"
-                          className="text-blue-500 hover:underline"
-                        >
-                          Category
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          href="/my-blogs"
-                          className="text-blue-500 hover:underline"
-                        >
-                          Blog
-                        </Link>
-                      </li>
-                    </ul>
-                    <Button
-                      onClick={handleLogout}
-                      variant="destructive"
-                      className={cn("mt-4 w-full hover:cursor-pointer")}
-                    >
-                      Logout
+          <div className="flex items-center space-x-4 ">
+            <DarkLightSwitch />
+            {user ? (
+              <div>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button className="bg-white text-blue-500 hover:bg-gray-100 hover:text-blue-700">
+                      <FaRegUser className="mr-2" />
+                      Profile
                     </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </div>
-          ) : (
-            <div>
-              <Link href="/login">Login</Link>
-            </div>
-          )}
+                  </PopoverTrigger>
+                  <PopoverContent className="w-48">
+                    <div className="text-center">
+                      <ul className="space-y-2">
+                        <li>
+                          <Link
+                            href="/category"
+                            className="text-blue-500 hover:underline"
+                          >
+                            Category
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/my-blogs"
+                            className="text-blue-500 hover:underline"
+                          >
+                            Blog
+                          </Link>
+                        </li>
+                      </ul>
+                      <Button
+                        onClick={handleLogout}
+                        variant="destructive"
+                        className={cn("mt-4 w-full hover:cursor-pointer")}
+                      >
+                        Logout
+                      </Button>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            ) : (
+              <div>
+                <Link href="/login">Login</Link>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
